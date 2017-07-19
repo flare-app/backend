cca358f8a1bddf3b04f52a2aec370f2fce9768ff
Johannes Schneider <johannes.schneider@student.hpi.de>
2017-07-19 18:43:27 +0200

    added rest route handler

--- src/main/java/de/flare/http/route/execution/RestRouteHandler.java
+++ src/main/java/de/flare/http/route/execution/RestRouteHandler.java
@@ -1,4 +1,21 @@
 package de.flare.http.route.execution;
 
+import com.sun.istack.internal.NotNull;
+import de.flare.http.request.RestRequest;
+import de.flare.http.response.RestResponse;
+import de.flare.http.route.RestRoute;
+
+/**
+ * This interface offers a method to handle rest requests.
+ */
+@FunctionalInterface
 public interface RestRouteHandler {
+
+	/**
+	 * This method handles the execution of a specific rest route.
+	 * @param route the route, which was called
+	 * @param request the incoming request
+	 * @param response the outgoing response
+	 */
+	void handle(@NotNull RestRoute route, @NotNull RestRequest request, @NotNull RestResponse response);
 }
